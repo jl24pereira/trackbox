f02c20a18168847220c5b095aed8b695c6b32758
José Luis Pereira <jl24pereira@gmail.com>
2026-01-14 02:58:59 -0600

    feat: se agrega setup para docker compose y modulo container-service

---
+++ container-service/src/test/java/com/kalapa/trackbox/container_service/ContainerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalapa.trackbox.container_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
